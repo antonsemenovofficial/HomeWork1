dbb632ebe4c7e7e5629ea06730ab06198a0f3a4e
antonsemenovofficial <118879810+antonsemenovofficial@users.noreply.github.com>
2023-07-07 19:31:19 +0300

    Первое задание

---  lesson_2_task_0.py
+++  lesson_2_task_0.py
@@ -1,8 +1,5 @@
-my_age = input("Введите ваш возраст: ")
-
-print("Ваш возраст: " + my_age)
+my_age = int(input())
+print('Ваш возраст:',my_age)
 
 my_age = my_age + 1
-
-
-print("Обновленный возраст " + my_age)
+print("Ваш возраст через год:",my_age)
